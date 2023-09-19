7ba948fd4544b90f78fd8957d260f3abf75a7938
John kim <jeo1kim@gmail.com>
2023-09-18 23:09:12 -0700

    main color done

--- example/lib/theme/theme_constants.dart
+++ example/lib/theme/theme_constants.dart
@@ -7,7 +7,7 @@ const kContentColorLightTheme = Color(0xFF1D1D35);
 const kContentColorDarkTheme = Color(0xFFF5FCF9);
 const kWarninngColor = Color(0xFFF3BB1C);
 const kErrorColor = Color(0xFFF03738);
-const kBackgroundColor = Color(0xFFFFE8A3);
+const kBackgroundColor = Color(0xFFF5E5CF);
 
 const kDefaultPadding = 20.0;
 
